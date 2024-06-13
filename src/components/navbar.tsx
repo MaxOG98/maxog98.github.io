cbcb766cafdce1e1e201932b4d15489fbff2bf65
MaxOG98 <oggioni.massimiliano@gmail.com>
2024-06-13 13:18:05 +0200

    UPDATE!

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,21 +4,17 @@ export default function Navbar() {
   const [toggled, setToggled] = useState<boolean>(false);
 
   return (
-    <nav className="flex items-center justify-between bg-slate-800 px-8">
-      <img src="/logonmm.svg" height={48} width={48} />
+    <nav className="flex items-center justify-center bg-slate-800 px-8">
+      {/* <img src="/logonmm.svg" height={48} width={48} /> */}
       <div className="my-8 flex flex-col items-center">
-        <h1 className="text-2xl md:text-3xl xl:text-5xl">
-          <span className="text-accent">n</span>ina{" "}
-          <span className="text-accent">m</span>adison{" "}
-          <span className="text-accent">m</span>usic
-        </h1>
-        <p className="mt-2">Composer for Films and Videogames</p>
+        <h1 className="text-3xl md:text-4xl xl:text-6xl">nina madison</h1>
+        <p className="mt-2 font-medium">Composer for Films and Videogames</p>
       </div>
-      <div className="flex h-6 w-6 cursor-pointer flex-col items-center justify-center space-y-1">
+      {/* <div className="flex h-6 w-6 cursor-pointer flex-col items-center justify-center space-y-1">
         <span className="block h-1 w-6 rounded-lg bg-white"></span>
         <span className="block h-1 w-6 rounded-lg bg-white"></span>
         <span className="block h-1 w-6 rounded-lg bg-white"></span>
-      </div>
+      </div> */}
     </nav>
   );
 }
