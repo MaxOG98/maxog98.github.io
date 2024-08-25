f43e4c3d6c730bebca1593435dba37e1cfdab6ea
MaxOG98 <oggioni.massimiliano@gmail.com>
2024-08-25 12:09:52 +0200

    refactor: Update Astro configuration and dependencies with google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://maxog98.github.io/",
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
